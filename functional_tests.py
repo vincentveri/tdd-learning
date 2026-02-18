9d4d4dda94419e9daecb05c9881d7c8bb2532766
Vincent <vincent.veri@gmail.com>
2026-02-18 16:26:46 +0100

    crea un test sfruttando il modulo nativo unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'successfully' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # John has heard about a new online to-do app. 
+        # He goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+        # he is invited to enter a to-do item straight away
+
+        # he types "buy peacock feathers" into a text box
+
+        # when he hits enter, the page updates, and now the page lists
+        # "1: buy peacock feathers" as an item in a to-do list
+
+        # there is still a text box inviting he to add another item
+
+        # the page updates again and now shows both items on her list
+
+        # edith wonders whether the site will remember her list.
+        # Then she sees that the site has generated a unique URL for her -- there is some explanatory text to that effect.
+
+        # she visits that URL - her to-do list is still there.
+
+        # satisfied, she goes back to sleep
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
