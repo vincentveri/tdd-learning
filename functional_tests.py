b5e309f0bc8fcd0576404d4c991600a1f49695e6
Vincent <vincent.veri@gmail.com>
2026-02-19 15:20:21 +0100

    Refactor della view homepage per usare un template

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-
 
     def tearDown(self):
         self.browser.quit()
@@ -17,16 +19,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test')
+        header_text = self.browser.find_element(by=By.TAG_NAME, value='h1').text
+        self.assertIn('To-Do', header_text)
 
         # he is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # he types "buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # when he hits enter, the page updates, and now the page lists
         # "1: buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(by=By.ID, value='id_list_table')
+        rows = table.find_elements(by=By.TAG_NAME, value='tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # there is still a text box inviting he to add another item
+        self.fail("Finish the test!")
 
         # the page updates again and now shows both items on her list
 
