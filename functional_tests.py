b1e9f1aa7b59a6ca574c6c2d01fec4f8c22bf792
Vincent <vincent.veri@gmail.com>
2026-02-19 15:47:03 +0100

    correggi il functional test

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(by=By.ID, value='id_list_table')
+        rows = table.find_elements(by=By.TAG_NAME, value='tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # John has heard about a new online to-do app. 
         # He goes to check out its homepage
@@ -36,18 +41,20 @@ class NewVisitorTest(unittest.TestCase):
         # "1: buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(by=By.ID, value='id_list_table')
-        rows = table.find_elements(by=By.TAG_NAME, value='tr')
-        self.assertIn(
-            '1: Buy peacock feathers',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # there is still a text box inviting he to add another item
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # the page updates again and now shows both items on her list
+        self.fail("Finish the test!")
 
         # edith wonders whether the site will remember her list.
         # Then she sees that the site has generated a unique URL for her -- there is some explanatory text to that effect.
