70e0c6de28fbab9d2fe4889e380b8e443188523c
Vincent <vincent.veri@gmail.com>
2026-02-19 16:21:44 +0100

    testa redirect e salvataggio singolo valido elemento

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    item_text = request.POST.get('item_text', '')
+
+    new_item_text = request.POST.get('item_text', '')
+    if request.method == 'POST' and new_item_text != '':
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+
     return render(request, 'lists/home.html', {
-        'items': [item_text]
+        'items': [new_item_text]
     })
